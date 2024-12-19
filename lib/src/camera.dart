833c53ed365230ed4e6baf1866065eddb2dadef6
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2024-12-19 11:24:54 +0900

    hasValues의 deprecated에 의한 변경

--- lib/src/camera.dart
+++ lib/src/camera.dart
@@ -59,7 +59,7 @@ class CameraPosition {
   }
 
   @override
-  int get hashCode => hashValues(bearing, target, tilt, zoom);
+  int get hashCode => Object.hash(target, zoom, tilt, bearing);
 
   @override
   String toString() =>
